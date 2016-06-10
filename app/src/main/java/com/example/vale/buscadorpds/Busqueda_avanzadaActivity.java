af8b96d9735b468b2e99180059969cefcb47ff15
ValentinaRe <va.reyescordova@gmail.com>
2016-06-10 00:05:59 -0400

    cliente buscador proyecto contacto empresarial

---
+++ app/src/main/java/com/example/vale/buscadorpds/Busqueda_avanzadaActivity.java
@@ -0,0 +1,13 @@
+package com.example.vale.buscadorpds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Busqueda_avanzadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_avanzada);
+    }
+}
---
+++ app/src/main/java/com/example/vale/buscadorpds/Busqueda_simpleActivity.java
@@ -0,0 +1,21 @@
+package com.example.vale.buscadorpds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Busqueda_simpleActivity extends AppCompatActivity {
+    String NAMESPACE="";
+    String URL="";
+    String SOAP_ACTION="";
+    String METHOD_NAME="";
+
+    EditText texto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_simple);{
+
+        }
+    }
+}
